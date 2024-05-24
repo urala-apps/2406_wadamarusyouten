4d5eaeb00b624549d9c44307304ae2840c8e0569
urala-apps <139664292+urala-apps@users.noreply.github.com>
2024-05-24 12:57:31 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,112 @@
+/*
+<JS>
+店名
+左メニュー名
+右メニュー名
+左メニュー画像（ファイル名：店名_左_メニュー名）
+右メニュー画像（ファイル名：店名_右_メニュー名）
+
+<CSS>
+背景画像
+左ボタンカラーコード
+右ボタンカラーコード
+
+CSSのカラーコード、リンク設定
+
+LINEログイン作成
+Githubリポジトリ作成
+スプレッドシート作成（GASデプロイ）
+LookerStudio設定（リンク共有）
+
+アニメーションのパターン3つほど
+
+looker　一覧作成
+*/
+
+////////////////////////////////////////////////////////////
+const shopName = "はせや";
+const menu_left = "浅利チャンジャとごぼうの韓国風釜飯";
+const menu_right = "ほたてバター釜飯";
+
+const menu_pic_left = "2406_vote_menu_left.png";
+const menu_pic_right = "2406_vote_menu_right.png";
+
+const btn_left = "2406_vote_btn_left.png";
+const btn_right = "2406_vote_btn_right.png";
+
+const _liffId = "2003940861-D8aypzR1";
+const GASUrl = "https://script.google.com/macros/s/AKfycbxLSb3L_YFi_NfbZPp_KGHhLRcsi7WlNhG6CWx2EDSAJgFyFAymEL98wEe2m_pO3oXs/exec";
+////////////////////////////////////////////////////////////
+
+liff.init({
+  liffId: _liffId
+}).catch((err) => {
+  console.log(err);
+});
+
+liff.ready.then(() => {
+  if (!liff.isLoggedIn()) {
+    liff.login();
+  }
+  const idToken = liff.getDecodedIDToken();
+  const userId = idToken.sub;
+  const userName = idToken.name;
+  const userPic = idToken.picture;
+  $('form').append(`<input type="hidden" name="userId" value="${userId}">`);
+  $('form').append(`<input type="hidden" name="userName" value="${userName}">`);
+  $('form').append(`<input type="hidden" name="userPic" value="${userPic}">`);
+
+  $('ul').append('<input type="submit" class="hide">');
+
+
+$('.btn-items').append('<li class="inner"><label><div class="flexItem flexItemTop"><img class="flexItem_left btn_animate05" src="'+ menu_pic_left +'"></div><div class="flexItem flexItemBottom"><button id="btn_id_left" type="submit" name="voteLeft"><img class="flexItem_left btn_animate00" src="'+ btn_left +'"></button></div></label></li>');
+$('.btn-items').append('<li class="inner"><label><div class="flexItem flexItemTop"><img class="flexItem_left btn_animate05_R" src="'+ menu_pic_right +'"></div><div class="flexItem flexItemBottom"><button id="btn_id_right" type="submit" name="voteRight"><img class="flexItem_left btn_animate00" src="'+ btn_right +'"></button></div></label></li>');
+
+});
+
+
+// $('.btn-items').append('<li class="inner"><label><div class="flexItem flexItemTop"><img class="flexItem_left btn_animate05" src="' + menu_pic_left + '"></div><div class="flexItem flexItemBottom"><button id="btn_id_left" type="submit" name="voteLeft"><img class="flexItem_left btn_animate00" src="' + btn_left + '"></button></div></label></li>');
+// $('.btn-items').append('<li class="inner"><label><div class="flexItem flexItemTop"><img class="flexItem_left btn_animate05_R" src="' + menu_pic_right + '"></div><div class="flexItem flexItemBottom"><button id="btn_id_right" type="submit" name="voteRight"><img class="flexItem_left btn_animate00" src="' + btn_right + '"></button></div></label></li>');
+
+// document.getElementById("btn_id_left").innerHTML = menu_left + "に<br>投票する";
+// document.getElementById("btn_id_right").innerHTML = menu_right + "に<br>投票する";
+
+const click_id_left = "btn_id_left";
+const click_id_right = "btn_id_right";
+
+
+$('form').submit(function (event) {
+  event.preventDefault();
+
+  const click_id = event.originalEvent.submitter.id;//⓵
+  let voteText = "";
+
+  if (click_id === click_id_left) {
+    console.log('ok');
+    voteText = menu_left;
+  } else if (click_id === click_id_right) {
+    console.log('cancel');
+    voteText = menu_right;
+  }
+
+  // alert(voteText + "に投票しますか？");
+  let data = $('form').serializeArray();
+  data.push({ name: 'menu', value: voteText });
+  data.push({ name: 'shop', value: shopName });
+
+  // GASへ送信
+  $.post(GASUrl, data);
+
+  // alert("投票しました！");
+  Swal.fire({
+    title: "投票完了！",
+    text: shopName + "『 " + voteText + " 』\nに投票しました。",
+    icon: "success"
+  });
+
+  // 投票したら、LINEのメッセージを送信する
+
+
+  // liff.closeWindow();
+});
+
